fc53c3865fad6bddd9f48ffc538db21c7cdcc9cf
dinhhaii <dinhhhaii@gmail.com>
2019-11-03 02:28:46 +0700

    update font

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import "./Login.css";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import { loginUser } from "../../actions/actions";
