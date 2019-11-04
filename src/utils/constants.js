b93bada479cae804c7f58ca12d9636148bb1412d
dinhhaii <dinhhhaii@gmail.com>
2019-11-05 04:25:03 +0700

    fix api_url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const TOKEN_USER = "ACCESS_TOKEN";
 export const OAUTH_GOOGLE = "https://oauth2.googleapis.com/tokeninfo?id_token=";
-export const API_URL = "http://localhost:3000";
+export const API_URL = "https://dinhhai-caro-server.herokuapp.com/";
 export const WIDTH = 20;
 export const HEIGHT = 20;
